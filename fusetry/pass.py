d824faa506894096402553391ac7e2694f482a93
root <ricardodiaas19@gmail.com>
2020-01-12 02:46:13 +0000

    modified:   pass.py 	new file:   test.csv

--- fusetry/pass.py
+++ fusetry/pass.py
@@ -1,43 +1,63 @@
 import easygui
 import sys
 import smtplib, ssl
+import csv
 from random import randrange
 
+def sendmail(name,email,p):
+        smtp_server = "smtp.gmail.com"
+        port = 587  # For starttls
+        sender_email = "tecnologiaseguranca2020@gmail.com"
+        password = "ricardomateus"
+        receiver_email = email
+        #p = randrange(1,9999)
+        message = " Subject: Hi there "+ name +"! to acess folder this is the password you need "+ str(p) +", thank you have a nice day!"
 
-smtp_server = "smtp.gmail.com"
-port = 587  # For starttls
-sender_email = "tecnologiaseguranca2020@gmail.com"
-password = "ricardomateus"
-receiver_email = sender_email
+        # Create a secure SSL context
+        context = ssl.create_default_context()
+
+        # Try to log in to server and send email
+        try:
+            server = smtplib.SMTP(smtp_server,port)
+            server.ehlo() # Can be omitted
+            server.starttls(context=context) # Secure the connection
+            server.ehlo() # Can be omitted
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
+            # TODO: Send email here
+        except Exception as e:
+            # Print any error messages to stdout
+            print(e)
+        finally:
+            server.quit() 
+    
+
+with open('test.csv', newline='') as csvfile:
+    data = list(csv.reader(csvfile))
+
+for i in data: 
+    print(i[0]) 
+    
 p = randrange(1,9999)
-message = " Subject: Hi there! to acess folder this is the password you need "+ str(p) +", thank you have a nice day!"
 
-# Create a secure SSL context
-context = ssl.create_default_context()
-
-# Try to log in to server and send email
-try:
-    server = smtplib.SMTP(smtp_server,port)
-    server.ehlo() # Can be omitted
-    server.starttls(context=context) # Secure the connection
-    server.ehlo() # Can be omitted
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
-    # TODO: Send email here
-except Exception as e:
-    # Print any error messages to stdout
-    print(e)
-finally:
-    server.quit() 
+myuser = easygui.enterbox("Wich User are you?")
 
 
-myvar = easygui.enterbox("Enter Password?")
-print(p)
-if(myvar == str(p)):
+
+for i in data:
+    if(i[0]==myuser):
+        sendmail(i[0],i[1],p)
+
+        
+fieldNames=['Nome','Password']
+myvar = easygui.multenterbox('Fill in the blanks', 'Authorizer', fieldNames)               
+print("this is p-->",p) 
+print("pass-->",myvar[1])          
+if(myvar[1] == str(p)):
     print("Hell", myvar)
     sys.exit(1)
 else:
-    print(myvar)
+    #print(myvar)
     sys.exit(-1)
 
    
